ee71a26b3051b82f4f6448a2fc3e5c1da971dbfc
Muharem Hrnjadovic <muharem@linux.com>
2016-06-24 19:57:31 +0200

    work in progress

--- src/scc.erl
+++ src/scc.erl
@@ -1,4 +1,5 @@
 -module(scc).
+-include_lib("eunit/include/eunit.hrl").
 
 %% API exports
 -export([calculate_jumps/1]).
@@ -7,9 +8,15 @@
 %% API functions
 %%====================================================================
 calculate_jumps([]) -> erlang:error(badarg);
-calculate_jumps(_A) -> nil.
+calculate_jumps(A) -> do_calculate_jumps(A, 0, length(A), 0).
 
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+do_calculate_jumps(_, _, Len, Jumps) when Jumps > Len -> never;
+do_calculate_jumps(_, Idx, Len, Jumps) when Idx >= Len -> {ok, Jumps};
+do_calculate_jumps(A, Idx, Len, Jumps) ->
+    Next = Idx + lists:nth(Idx+1, A),
+    ?debugFmt("next: ~p~n", [Next]),
+    do_calculate_jumps(A, Next, Len, Jumps + 1).
