7a8a8f6b05e001e503ef6ad04e167474e25ade74
Muharem Hrnjadovic <muharem@linux.com>
2016-06-26 13:03:20 +0200

    turned off debug output.

--- src/scc.erl
+++ src/scc.erl
@@ -1,4 +1,5 @@
 -module(scc).
+-define(NODEBUG, true).
 -include_lib("eunit/include/eunit.hrl").
 
 %% API exports
@@ -12,7 +13,7 @@
 -spec calculate_jumps(A::nonempty_list(value())) -> { ok, jumps()} | never.
 calculate_jumps(A) when length(A) < 1; length(A) > 100000 -> erlang:error(badarg);
 calculate_jumps(A) ->
-    % ?debugFmt("~nA: ~p", [A]),
+    ?debugFmt("~nA: ~p", [A]),
     do_calculate_jumps(A, 0, length(A), 0).
 
 
@@ -27,5 +28,5 @@ do_calculate_jumps(_, _, Len_A, Jumps) when Jumps > Len_A -> never;
 do_calculate_jumps(_, K, Len_A, Jumps) when K >= Len_A; K < 0 -> {ok, Jumps};
 do_calculate_jumps(A, K, Len_A, Jumps) ->
     N = K + lists:nth(K+1, A),
-    % ?debugFmt("K: ~p, N: ~p", [K, N]),
+    ?debugFmt("K: ~p, N: ~p", [K, N]),
     do_calculate_jumps(A, N, Len_A, Jumps+1).
