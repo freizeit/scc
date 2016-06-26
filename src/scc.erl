3e7025e13e99655029fd7fbaa79d1d38d5650f57
Muharem Hrnjadovic <muharem@linux.com>
2016-06-26 11:51:29 +0200

    first solution, needs polish

--- src/scc.erl
+++ src/scc.erl
@@ -14,9 +14,9 @@ calculate_jumps(A) -> do_calculate_jumps(A, 0, length(A), 0).
 %%====================================================================
 %% Internal functions
 %%====================================================================
-do_calculate_jumps(_, _, Len, Jumps) when Jumps > Len -> never;
-do_calculate_jumps(_, Idx, Len, Jumps) when Idx >= Len -> {ok, Jumps};
-do_calculate_jumps(A, Idx, Len, Jumps) ->
-    Next = Idx + lists:nth(Idx+1, A),
-    ?debugFmt("next: ~p~n", [Next]),
-    do_calculate_jumps(A, Next, Len, Jumps + 1).
+do_calculate_jumps(_, _, Len_A, Jumps) when Jumps > Len_A -> never;
+do_calculate_jumps(_, K, Len_A, Jumps) when K >= Len_A; K < 0 -> {ok, Jumps};
+do_calculate_jumps(A, K, Len_A, Jumps) ->
+    N = K + lists:nth(K+1, A),
+    ?debugFmt("A: ~p, K: ~p, N: ~p~n", [A, K, N]),
+    do_calculate_jumps(A, N, Len_A, Jumps+1).
--- test/scc_test.erl
+++ test/scc_test.erl
@@ -2,11 +2,29 @@
 -include_lib("eunit/include/eunit.hrl").
 
 
-scc_0_test() ->
+scc_empty_list_test() ->
     ?assertError(badarg, scc:calculate_jumps([])).
 
-scc_1_test() ->
+scc_never_test() ->
     ?assertEqual(never, scc:calculate_jumps([1, 1, -1, 1])).
 
-scc_2_test() ->
+scc_4out_right_test() ->
     ?assertEqual({ok, 4}, scc:calculate_jumps([2, 3, -1, 1, 3])).
+
+scc_never_lower_boundary_test() ->
+    ?assertEqual(never, scc:calculate_jumps([0])).
+
+scc_never_upper_boundary_test() ->
+    ?assertEqual(never, scc:calculate_jumps([1, 1, 1, -3])).
+
+scc_4out_left_test() ->
+    ?assertEqual({ok, 4}, scc:calculate_jumps([2, 3, -1, 1, -5])).
+
+scc_1out_lower_boundary_right_test() ->
+    ?assertEqual({ok, 1}, scc:calculate_jumps([1])).
+
+scc_1out_lower_boundary_left_test() ->
+    ?assertEqual({ok, 1}, scc:calculate_jumps([-1])).
+
+scc_4out_upper_boundary_right_test() ->
+    ?assertEqual({ok, 5}, scc:calculate_jumps([1, 1, 1, 1, 1])).
