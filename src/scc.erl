d3d27486a4be6351a21c5ff0b52611e083b10c92
Muharem Hrnjadovic <muharem@linux.com>
2016-06-26 12:31:41 +0200

    cleaned up tests

--- src/scc.erl
+++ src/scc.erl
@@ -7,16 +7,25 @@
 %%====================================================================
 %% API functions
 %%====================================================================
-calculate_jumps([]) -> erlang:error(badarg);
-calculate_jumps(A) -> do_calculate_jumps(A, 0, length(A), 0).
+-type value() :: -1000000..1000000.
+-type jumps() :: 1..100001.
+-spec calculate_jumps(A::nonempty_list(value())) -> { ok, jumps()} | never.
+calculate_jumps(A) when length(A) < 1; length(A) > 100000 -> erlang:error(badarg);
+calculate_jumps(A) ->
+    ?debugFmt("~nA: ~p", [A]),
+    do_calculate_jumps(A, 0, length(A), 0).
 
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+-spec do_calculate_jumps(A::nonempty_list(value()), K::0..99999,
+                         Len_A::1..100000, Jumps::0..100001) -> { ok, jumps()}
+                                                                | never.
+
 do_calculate_jumps(_, _, Len_A, Jumps) when Jumps > Len_A -> never;
 do_calculate_jumps(_, K, Len_A, Jumps) when K >= Len_A; K < 0 -> {ok, Jumps};
 do_calculate_jumps(A, K, Len_A, Jumps) ->
     N = K + lists:nth(K+1, A),
-    ?debugFmt("A: ~p, K: ~p, N: ~p~n", [A, K, N]),
+    ?debugFmt("K: ~p, N: ~p", [K, N]),
     do_calculate_jumps(A, N, Len_A, Jumps+1).
--- test/scc_test.erl
+++ test/scc_test.erl
@@ -2,14 +2,20 @@
 -include_lib("eunit/include/eunit.hrl").
 
 
+%%====================================================================
+%% error cases
+%%====================================================================
 scc_empty_list_test() ->
     ?assertError(badarg, scc:calculate_jumps([])).
 
-scc_never_test() ->
-    ?assertEqual(never, scc:calculate_jumps([1, 1, -1, 1])).
+scc_list_too_big_test() ->
+    ?assertError(badarg, scc:calculate_jumps(lists:seq(1,100001))).
 
-scc_4out_right_test() ->
-    ?assertEqual({ok, 4}, scc:calculate_jumps([2, 3, -1, 1, 3])).
+%%====================================================================
+%% test cases resulting in never
+%%====================================================================
+scc_never_case_from_doc_test() ->
+    ?assertEqual(never, scc:calculate_jumps([1, 1, -1, 1])).
 
 scc_never_lower_boundary_test() ->
     ?assertEqual(never, scc:calculate_jumps([0])).
@@ -17,14 +23,20 @@ scc_never_lower_boundary_test() ->
 scc_never_upper_boundary_test() ->
     ?assertEqual(never, scc:calculate_jumps([1, 1, 1, -3])).
 
-scc_4out_left_test() ->
-    ?assertEqual({ok, 4}, scc:calculate_jumps([2, 3, -1, 1, -5])).
+%%====================================================================
+%% test cases resulting in {ok, N}
+%%====================================================================
+scc_4_and_out_case_from_doc_test() ->
+    ?assertEqual({ok, 4}, scc:calculate_jumps([2, 3, -1, 1, 3])).
 
-scc_1out_lower_boundary_right_test() ->
+scc_1_and_out_lower_boundary_right_test() ->
     ?assertEqual({ok, 1}, scc:calculate_jumps([1])).
 
-scc_1out_lower_boundary_left_test() ->
+scc_1_and_out_lower_boundary_left_test() ->
     ?assertEqual({ok, 1}, scc:calculate_jumps([-1])).
 
-scc_4out_upper_boundary_right_test() ->
+scc_5_and_out_upper_boundary_left_test() ->
+    ?assertEqual({ok, 5}, scc:calculate_jumps([1, 1, 1, 1, -5])).
+
+scc_5_and_out_upper_boundary_right_test() ->
     ?assertEqual({ok, 5}, scc:calculate_jumps([1, 1, 1, 1, 1])).
